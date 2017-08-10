42e3287d51a67e723b2e91e8817d23a4e589087e
szgxzdk <100041367@qq.com>
2017-08-10 15:36:17 +0800

    remove all

--- cmd_dvfs.sh
+++
@@ -1,5 +0,0 @@
-~/cmp_test/cmp_sim_dvfs_RPM/sim-outorder \
-        -redir:sim ./sim_result/hello_sim.txt \
-        -redir:dump ./sim_result/a.out \
-        -config config_mesh_ooo_xy_dvfs \
-		-max:barrier 0 -max:inst 10000000 hello.BNC
